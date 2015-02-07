1b1e8cf29814f28c0d55f5f8b54371053fd6b56f
Zander Allen <ztrain01@gmail.com>
2015-02-07 13:12:58 -0700

    adding up or down vote

--- controller.js
+++ controller.js
@@ -1,5 +1,23 @@
 var app = angular.module('reddit');
 
 app.controller('PostController', function($scope) {
-  $scope.test = 'Welcome to Reddit!'
-}
+  $scope.test = 'Welcome to Reddit!';
+  $scope.posts = [];
+  $scope.nums = [1,2,3,4,5,6];
+  
+  $scope.addPost = function() {
+    
+    $scope.newPost.timeStamp = Date.now();
+    $scope.newPost.karma = 0;
+    $scope.newPost.comments = [];
+    console.log($scope.newPost);
+    $scope.posts.push($scope.newPost)
+    $scope.reset();
+  }
+  
+  $scope.reset = function() {
+    $scope.newPost = {};
+  }
+  
+  
+});
